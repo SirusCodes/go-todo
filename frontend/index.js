e341cad6490354d54db5996291a8fcb37e2eb6b7
SirusCodes <darshandrander@gmail.com>
2021-08-03 00:36:18 +0530

    feat: saving todos

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,9 @@
+const ENDPOINT = "http://127.0.0.1:8000";
+
 const username = localStorage.getItem('username');
 const token = localStorage.getItem('token');
+const logoutBtn = document.getElementById("logout-btn");
+const todoText = document.getElementById("todo-text");
 
 window.addEventListener('load', () => {
     if (!(username && token)) {
@@ -8,9 +12,54 @@ window.addEventListener('load', () => {
     document.getElementById("hello-user").innerHTML = `Hello, ${username}`;
 });
 
-const logoutBtn = document.getElementById("logout-btn");
-
 logoutBtn.addEventListener('click', () => {
     localStorage.clear();
     window.location = "http://127.0.0.1:5500/frontend/authenticate.html"
 });
+
+todoText.addEventListener('keyup', async (e) => {
+    if (e.key === "Enter") {
+        await sendTodo();
+    }
+});
+
+async function refreshTokens() {
+    const response = await fetch(`${ENDPOINT}/refresh`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            username: username,
+            refresh_token: localStorage.getItem('refresh_token'),
+        })
+    });
+
+    if (response.status === 201) {
+        const result = await response.json();
+        localStorage.setItem('token', result.data.token);
+        token = result.data.token;
+        sendTodo();
+    }
+}
+
+async function sendTodo() {
+    const response = await fetch(`${ENDPOINT}/todo`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            task: todoText.value
+        })
+    });
+
+    if (response.status === 200) {
+        todoText.value = "";
+    } else if (response.status === 401) {
+        refreshTokens();
+    }
+
+}
--- routers/routers.go
+++ routers/routers.go
@@ -14,6 +14,6 @@ func SetRouters(app *fiber.App) {
 	app.Get("/todos", middleware.HandleJWTAuth(), handlers.GetTodos)
 	app.Get("/todo/:id", middleware.HandleJWTAuth(), handlers.GetTodo)
 	app.Post("/todo", middleware.HandleJWTAuth(), handlers.SaveTodo)
-	app.Put("/todo", middleware.HandleJWTAuth(), handlers.UpdateTodo)
+	app.Patch("/todo", middleware.HandleJWTAuth(), handlers.UpdateTodo)
 	app.Delete("/todo/:id", middleware.HandleJWTAuth(), handlers.DeleteTodo)
 }
